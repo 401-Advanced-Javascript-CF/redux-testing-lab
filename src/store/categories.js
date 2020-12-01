62edc95f98d87ee2e38f040c1c43e5527ac7430a
BryantDavis1986 <Bryant.Davis@hotmail.com>
2020-11-30 21:40:22 -0800

    barely started

---
+++ src/store/categories.js
@@ -0,0 +1,19 @@
+const initialState = {
+    
+  }
+  
+  const selectCategory = () => {
+    return {
+  
+    }
+  }
+  
+  
+  // What should be done to our initial state?
+  const categoryReducer = (state = initialState, action) => {
+  
+  
+    // this returns the new state.
+  }
+  
+  export default categoryReducer;
