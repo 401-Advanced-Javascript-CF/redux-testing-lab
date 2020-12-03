2882f6f2fd312d4d40c4b5ff5fa963d338abefc4
BryantDavis1986 <Bryant.Davis@hotmail.com>
2020-12-02 17:11:12 -0800

    finish day1

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+
+
+function Header(props){
+
+    return (
+        <header>
+            <h2>Pups - R - Us</h2>
+        </header>
+    )
+}
+
+export default Header;
--- src/store/products.js
+++ src/store/products.js
@@ -1,9 +1,13 @@
 const initialState = {
-    category: [{name: 'Rmpage', inventoryCount: 1, price: 10, category: 'Staffordshire Terrier' }],
-    active: ''
+    products: [{name: 'Rampage', inventoryCount: 1, price: 10, category: 'Staffordshire Terrier' }],
   }
   
-  
+  export const selectProducts = (stuff) => {
+    return {
+      type: 'Products',
+      payload: stuff
+    }
+  }
   // What should be done to our initial state?
   const productsReducer = (state = initialState, action) => {
     return state;
